c42381368b5a5384ed80e22c466c5b814d49f5e6
t nakai <t-nakai@nttpc.co.jp>
2017-12-04 16:50:21 +0000

    exam3 mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { host: 'https://polar-ravine-89153.herokuapp.com/' }
+config.action_mailer.default_url_options = { host: 'http://54.64.67.21/' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
