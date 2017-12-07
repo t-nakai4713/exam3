ff1f8c47b2d3d86384067f15f6b159bba8e3504e
t nakai <t-nakai@nttpc.co.jp>
2017-12-07 04:49:59 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 100.times do |n|
   email = Faker::Internet.email
   password = "password"
